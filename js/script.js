c6705e12dda87ed99dab0acc64d9f59ea9990721
EmanuelRigo <112441019+EmanuelRigo@users.noreply.github.com>
2023-04-29 13:30:31 -0300

    agregando funcion validar usuario y guardar datos

--- js/script.js
+++ js/script.js
@@ -1,5 +1,50 @@
+const usuarios = [
+  {
+    nombre: "Azul",
+    mail: "azulperez@mail.com",
+    pass: "azulcomoelmarazul",
+  },
+  {
+    nombre: "Betiana",
+    mail: "betidicarlo@mail.com",
+    pass: "sha23AWx!",
+  },
+  {
+    nombre: "Carlos",
+    mail: "lopezcarlosadrian@mail.com",
+    pass: "sanlore2002",
+  },
+];
+
 const registerButton = document.getElementById("register");
+const usernameInput = document.getElementById("usernameInput");
+const passwordInput = document.getElementById("passwordInput");
+const btnLogin = document.getElementById("btnLogin");
 
 registerButton.addEventListener("click", () => {
-  alert("hola");
+  alert("hola " + usernameInput.value);
 });
+
+function validarUsuario(usersDB, user, pass) {
+  let encontrado = usersDB.find((userDB) => usersDB.mail == user);
+
+  if (typeof encontrado === "undefined") {
+    return false;
+  } else {
+    if (encontrado.pass != pass) {
+      return false;
+    } else {
+      return encontrado;
+    }
+  }
+}
+
+function guardarDatos(usuarioDB, storage) {
+  const usuraio = {
+    name: usuarioDB.nombre,
+    user: usuarioDB.mail,
+    pass: usuarioDB.pass,
+  };
+
+  storage.setItem("usuario", JSON.stringify(usuraio));
+}
